af91525b8a9004d1b5095d527111dcbbfdad013e
Chaitanya Pappala <chaitanyakrishna.pappala@gmail.com>
2020-09-09 22:16:39 -0500

    fix(postgres): disable ssl

--- db/postgres/index.js
+++ db/postgres/index.js
@@ -6,6 +6,6 @@ dotenv.config()
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL || process.env.LOCAL_TP_DATABASE_URL,
-  ssl: !!process.env.DATABASE_URL
+  ssl: {rejectUnauthorized: false}
 })
 export default pool
