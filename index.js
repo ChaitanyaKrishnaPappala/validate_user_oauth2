fa91f46c7dab567dc9ed361ce9b9b56ea789154b
Chaitanya Pappala <chaitanyakrishna.pappala@gmail.com>
2020-09-10 23:30:32 -0500

    fix(oauth2): handle auth2 errors and update deploy file

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import bodyParser from 'body-parser'
 import oAuth2Server from 'node-oauth2-server'
 import model from './oauth/model.js'
 import {createUser, activateUser, getAllUsers, updateUserPassword} from './controller/user.js'
-import {login} from './controller/authentication.js'
 import {initializeNodeMailer} from './controller/email.js'
 
 dotenv.config()
@@ -14,11 +13,13 @@ const app = new express() //eslint-disable-line
 app.oauth = oAuth2Server({
   model,
   grants: ['password'],
-  debug: true
+  debug: false,
+  useErrorHandler: true
 })
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
+
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,PUT,DELETE')
@@ -30,17 +31,23 @@ app.use((req, res, next) => {
   return next()
 })
 
-// routes
 app.post('/user/register', createUser)
 app.get('/user/activate/:activation_token', activateUser)
-app.post('/user/login', app.oauth.grant(), login)
+app.post('/user/login', app.oauth.grant())
 app.post('/user/all', getAllUsers)
 app.put('/user/password', updateUserPassword)
-app.get('/dbconfig', (req, res, next) => {
-  res.status(200).json({
-    env: process.env
-  })
+// app.get('/dbconfig', (req, res, next) => {
+//   res.status(200).json({
+//     env: process.env
+//   })
+// })
+
+app.use(function (err, req, res, next) {
+  if (err instanceof Error) {
+    return res.status(err.code).send({message: err.toString(), error: err.error})
+  }
 })
+app.use(app.oauth.errorHandler())
 const port = process.env.PORT || 80
 
 app.listen(port, (err) => {
--- routes/index.js
+++
@@ -1,6 +0,0 @@
-import {createUser} from '../controller/user.js'
-
-export default function routes (router, auth) {
-  router.post('/user', auth.oauth.grant(), createUser)
-  return routes
-}
